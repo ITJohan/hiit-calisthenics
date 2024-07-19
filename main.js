d68f8cb7fc58de943672cf289dcd03abb283141a
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-07-19 15:35:01 +0200

    feat: add render and api middleware

--- main.js
+++ main.js
@@ -3,19 +3,45 @@ import * as db from './db.js';
 
 const PORT = 3000;
 
-const server = http.createServer(async (req, res) => {
-  const url = new URL(`http://${process.env.HOST ?? 'localhost'}${req.url}`);
-
-  try {
-    const dbResponse = await db.query('SELECT * FROM Exercises WHERE exercise_level = $1', [url.pathname.substring(1)]);
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(dbResponse.rows));
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.writeHead(400);
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {(req, res) => void} next
+ */
+function renderMiddleware(req, res, next) {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello world</h1>');
     res.end();
   }
-});
+
+  next();
+}
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {(req, res) => void} next
+ */
+async function apiMiddleware(req, res, next) {
+  if (req.url === '/api/exercises' && req.method === 'GET') {
+    try {
+      const response = await db.query('SELECT * FROM Exercises');
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(response.rows));
+      res.end();
+    } catch (err) {
+      console.error(err);
+      res.writeHead(400);
+      res.end();
+    }
+  }
+
+  next();
+}
+
+const server = http.createServer(
+  async (req, res) => await renderMiddleware(req, res, async () => await apiMiddleware(req, res, () => {}))
+);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
