7c45eaeee01f38fe328bacf0724473c81d04db89
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-07-19 15:41:58 +0200

    feat: list athletes

--- main.js
+++ main.js
@@ -8,11 +8,23 @@ const PORT = 3000;
  * @param {http.ServerResponse} res
  * @param {(req, res) => void} next
  */
-function renderMiddleware(req, res, next) {
+async function renderMiddleware(req, res, next) {
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello world</h1>');
-    res.end();
+    try {
+      const response = await db.query('SELECT * from Athletes');
+      const athletes = response.rows;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`
+        <h1>Athletes</h1>
+        <ul>
+          ${athletes.map((athlete) => `<li>${athlete.athlete_name}</li>`).join('')}
+        </ul>
+      `);
+      res.end();
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end();
+    }
   }
 
   next();
