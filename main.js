694bbb639c7b68bdf3aa5c5c9fb06fed13b79db6
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-07-19 10:20:54 +0200

    feat: replace db client with pool

--- main.js
+++ main.js
@@ -2,18 +2,17 @@
 
 import http from 'http';
 import pg from 'pg';
-const { Client } = pg;
+const { Pool } = pg;
 
 const PORT = 3000;
 
 const server = http.createServer(async (req, res) => {
   const url = new URL(`http://${process.env.HOST ?? 'localhost'}${req.url}`);
 
-  const client = new Client({ connectionString: process.env.POSTGRESQL_CONNECTION_STRING });
-  await client.connect();
+  const pool = new Pool({ connectionString: process.env.POSTGRESQL_CONNECTION_STRING });
 
   try {
-    const dbResponse = await client.query('SELECT * FROM Exercises WHERE exercise_level = $1', [
+    const dbResponse = await pool.query('SELECT * FROM Exercises WHERE exercise_level = $1', [
       url.pathname.substring(1),
     ]);
     res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -23,8 +22,6 @@ const server = http.createServer(async (req, res) => {
     console.error(err);
     res.writeHead(400);
     res.end();
-  } finally {
-    await client.end();
   }
 });
 
