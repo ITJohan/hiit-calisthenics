7dc8b5b8191b9488892a1b9d6ed1799d18d772f0
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-07-22 13:11:31 +0200

    feat: align form correctly

--- src/pages/create.js
+++ src/pages/create.js
@@ -4,18 +4,21 @@ export async function renderCreate() {
   const result = await db.query('SELECT * FROM Exercises');
   const exercises = result.rows;
 
-  console.log(exercises);
-
   return `
     <h2>Create new workout</h2>
     <main>
       <create-form>
         <form method="POST" action="/workout">
-          <label for="name">Name</label>
-          <input id="name" name="name" />
-          <select name="exercise">
-          ${exercises.map((exercise) => `<option value="${exercise.exercise_id}">${exercise.exercise_name}</option>`)}
-          </select>
+          <label>
+            Name
+            <input name="name" size="16" />
+          </label>
+          <label>
+            Exercise
+            <select name="exercise">
+            ${exercises.map((exercise) => `<option value="${exercise.exercise_id}">${exercise.exercise_name}</option>`)}
+            </select>
+          </label>
           <button>Submit</button>
         </form>
       </create-form>
