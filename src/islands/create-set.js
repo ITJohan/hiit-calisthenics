1befff09082e584ea01833d331f529296151a647
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-08-04 12:38:00 +0200

    refactor: add exercise method

--- src/islands/create-set.js
+++ src/islands/create-set.js
@@ -26,30 +26,32 @@ customElements.define('create-set', class CreateSet extends HTMLElement {
   handleEvent(/** @type {Event} */ event) {
     if (!(event.target instanceof HTMLButtonElement)) return;
 
-    if (event.target.matches('.add-exercise-btn')) {
-      const lastExerciseContainer = this.querySelector('.exercise-container:nth-last-child(2)');
-      const lastExerciseContainerCopy = lastExerciseContainer.cloneNode(true);
+    if (event.target.matches('.add-exercise-btn')) this.addExercise();
+  }
 
-      if (!(lastExerciseContainerCopy instanceof HTMLElement)) {
-        throw new Error('exerciseContainerCopy is not an instance of HTMLElement')
-      }
+  addExercise() {
+    const lastExerciseContainer = this.querySelector('.exercise-container:nth-last-child(2)');
+    const exerciseContainer = lastExerciseContainer.cloneNode(true);
 
-      const label = lastExerciseContainerCopy.querySelector('label');
-      const select = lastExerciseContainerCopy.querySelector('select');      
-
-      const labelParts = label.textContent.split(' ');
-      const selectIdParts = select.getAttribute('id').split('-');
-      
-      const newLabel = labelParts.map((part, index) => index === 1 ? String(Number(part) + 1) : part).join(' ');
-      const newSelectId = selectIdParts.map((part, index) => index === 3 ? String(Number(part) + 1) : part).join('-');
-
-      label.textContent = newLabel;
-      label.setAttribute('for', newSelectId);
-      select.setAttribute('id', newSelectId);
-      select.setAttribute('name', newSelectId);
-
-      lastExerciseContainer.after(lastExerciseContainerCopy);
+    if (!(exerciseContainer instanceof HTMLElement)) {
+      throw new Error('exerciseContainerCopy is not an instance of HTMLElement')
     }
+
+    const label = exerciseContainer.querySelector('label');
+    const select = exerciseContainer.querySelector('select');      
+
+    const labelParts = label.textContent.split(' ');
+    const selectIdParts = select.getAttribute('id').split('-');
+    
+    const newLabel = labelParts.map((part, index) => index === 1 ? String(Number(part) + 1) : part).join(' ');
+    const newSelectId = selectIdParts.map((part, index) => index === 3 ? String(Number(part) + 1) : part).join('-');
+
+    label.textContent = newLabel;
+    label.setAttribute('for', newSelectId);
+    select.setAttribute('id', newSelectId);
+    select.setAttribute('name', newSelectId);
+
+    lastExerciseContainer.after(exerciseContainer);
   }
 });
 
