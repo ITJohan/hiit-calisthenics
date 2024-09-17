7daa13fd6be6d32c8a9bebabfa35780fa6910895
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-09-17 18:40:37 +0200

    feat: add new app types

--- types.d.ts
+++ types.d.ts
@@ -1,52 +1,22 @@
-type Athlete = {
-  athlete_id: number,
-  athlete_email: string,
-  athlete_name: string,
-};
+type Exercise = {
+  id: string;
+  name: string;
+  type: 'rep' | 'sec';
+  equipment: ('band' | 'none' | 'parallel-bars' | 'rings' | 'box' | 'towel' | 'pull-up-bar' | 'weights' | 'wheel' | 'nordic-strap')[];
+}
+
+type Progression = {
+  id: string;
+  name: string;
+  exercises: {
+    id: string;
+    reps: number[];
+    rest: number;
+  }
+}
 
 type Workout = {
-  workout_id: number,
-  workout_name: string,
-  athlete_id: number,
-};
-
-type Category = 'pull' | 'push' | 'legs' | 'core';
-
-type Exercise = {
-  exercise_id: number,
-  exercise_name: string,
-  exercise_category: Category,
-  exercise_level: number,
-};
-
-type Log = {
-  log_id: number,
-  log_date: Date,
-  athlete_id: number,
-};
-
-type LogExercise = {
-  log_reps: number,
-  exercise_id: number,
-  log_id: number,
-};
-
-type WorkoutExercise = {
-  set_order: number,
-  exercise_order: number,
-  workout_id: number,
-  exercise_id: number,
-};
-
-interface ICreateWorkoutForm extends HTMLElement {}
-
-interface ICreateWorkoutSet extends HTMLElement {
-  setId: string,
-  resetExercises(): void,
+  id: string;
+  name: string;
+  progressions: string[];
 }
-
-interface ICreateSetExercise extends HTMLElement {
-  setId: string,
-  exerciseId: string,
-}
-
