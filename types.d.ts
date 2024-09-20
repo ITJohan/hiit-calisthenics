86f39500682151e0fa59d258633f3ba852fdec34
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-09-20 10:40:40 +0200

    feat: add category type

--- types.d.ts
+++ types.d.ts
@@ -1,43 +1,62 @@
 type Exercise = {
-	id: string;
-	name: string;
-	url: string;
-	type: "rep" | "sec";
-	equipment: (
-		| "band"
-		| "none"
-		| "parallel-bars"
-		| "rings"
-		| "box"
-		| "towel"
-		| "pull-up-bar"
-		| "weights"
-		| "wheel"
-		| "nordic-strap"
-	)[];
+  id: string;
+  name: string;
+  url: string;
+  type: "rep" | "sec";
+  equipment: (
+    | "band"
+    | "none"
+    | "parallel-bars"
+    | "rings"
+    | "box"
+    | "towel"
+    | "pull-up-bar"
+    | "weights"
+    | "wheel"
+    | "nordic-strap"
+  )[];
 };
 
 type ProgressionSet = {
-	exerciseId: string;
-	reps: number[];
-	rest: number;
+  exerciseId: string;
+  reps: number[];
+  rest: number;
 };
 
 type Progression = {
-	id: string;
-	name: string;
-	sets: ProgressionSet[];
+  id: string;
+  category: Category;
+  sets: ProgressionSet[];
 };
 
 type Workout = {
-	id: string;
-	name: string;
-	progressions: string[];
+  id: string;
+  name: string;
+  progressions: string[];
 };
 
+type Category =
+  | "shoulder-warmup"
+  | "spine-warmup"
+  | "wrist-warmup"
+  | "core-warmup"
+  | "back-warmup"
+  | "hold-warmup"
+  | "squat-warmup"
+  | "hinge-warmup"
+  | "pull-up"
+  | "squat"
+  | "dip"
+  | "hinge"
+  | "row"
+  | "push-up"
+  | "anti-extension"
+  | "anti-rotation"
+  | "extension";
+
 interface IRRSet {
-	name: string;
-	url: string;
-	reps: string[];
-	id: string;
+  name: string;
+  url: string;
+  reps: string[];
+  id: string;
 }
