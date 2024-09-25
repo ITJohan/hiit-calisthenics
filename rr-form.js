3ed8532891711c397b24d6e435f6c172a2b814e0
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-09-25 20:10:51 +0200

    feat: add rr-form"

---
+++ rr-form.js
@@ -0,0 +1,19 @@
+customElements.define(
+  "rr-form",
+  class RRForm extends HTMLElement {
+    connectedCallback() {
+      const form = this.querySelector("form");
+
+      if (!form) throw new Error("Need to wrap a form element");
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+
+        for (const [key, value] of formData) {
+          console.log(key, value);
+        }
+      });
+    }
+  },
+);
