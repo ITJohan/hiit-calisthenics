07d7182fee17a70877b992cfdc267abcad7cd63a
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-10-24 10:39:40 +0000

    feat: replace links with swipe icons

--- public/components/rr-set.js
+++ public/components/rr-set.js
@@ -1,11 +1,7 @@
 import { getExercise, getNextProgressionSet } from "../db.js";
 import { isCategory } from "../utils/type-guards.js";
 
-// UI inspo: https://www.youtube.com/watch?v=KrJZPrCqhWU&list=WL&index=13
-
-customElements.define(
-  "rr-set",
-  class RRSet extends HTMLElement {
+customElements.define("rr-set", class RRSet extends HTMLElement {
     get category() {
       const category = this.getAttribute("category");
       if (category === null) throw new Error("category is required");
@@ -29,6 +25,8 @@ customElements.define(
       this.update();
     }
 
+    // TODO: show previous reps as initial
+
     update() {
       const progressionSet = getNextProgressionSet(this.category);
       const exercise = getExercise(progressionSet.exerciseId);
@@ -39,28 +37,28 @@ customElements.define(
 
       this.innerHTML = `
         <h2><a href=${exercise.url} target="_blank">${exercise.name}</a></h2>
-        <img src="./assets/placeholder.jpg" width="50" />
         <div>
-          <label for="${id}-input">Reps: <span>0</span></label>
-          <input id="${id}-input" type="range" name="${id}" max="${progressionSet.max}" list="reps">
-          <datalist id="reps">
-            ${reps.map((rep) => `<option value="${rep}"></option>`).join("")}
-          </datalist>
+          <span>&lt;&lt;</span>
+          <rr-card>
+            <img src="./assets/placeholder.jpg" width="50" />
+            <label for="${id}-input">Reps: <time>0</time></label>
+            <input id="${id}-input" type="range" name="${id}" max="${progressionSet.max}" list="reps">
+            <datalist id="reps">
+              ${reps.map((rep) => `<option value="${rep}"></option>`).join("")}
+            </datalist>
+          </rr-card>
+          <span>&gt;&gt;</span>
         </div>
-        <nav>
-          <a href="#home">Previous</a>
-          <a href="#finish">Next</a>
-        </nav>
       `;
 
       const inputEl = this.querySelector("input");
-      const spanEl = this.querySelector("span");
-      if (inputEl === null || spanEl === null) {
+      const timeEl = this.querySelector("time");
+      if (inputEl === null || timeEl === null) {
         throw new Error("Could not query all elements");
       }
 
       inputEl.addEventListener("input", (event) => {
-        spanEl.textContent =
+        timeEl.textContent =
           /** @type {HTMLInputElement} */ (event.target).value;
       });
     }
