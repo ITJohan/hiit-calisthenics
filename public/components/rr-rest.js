275db742bc7c4b279eb8279c77ef064701e40d49
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-10-28 08:42:46 +0000

    feat: time attribute only as initial

--- public/components/rr-rest.js
+++ public/components/rr-rest.js
@@ -3,15 +3,7 @@ customElements.define(
   class RRRest extends HTMLElement {
     /** @type {HTMLTimeElement | null} */ timeElement = null;
     /** @type {number | undefined} */ intervalId = undefined;
-
-    get time() {
-      return this.getAttribute("time");
-    }
-    set time(time) {
-      time === null
-        ? this.removeAttribute("time")
-        : this.setAttribute("time", time);
-    }
+    /** @type {number} */ time = 0;
 
     get active() {
       return this.getAttribute("active") === "" ? true : false;
@@ -46,25 +38,22 @@ customElements.define(
       this.intervalId = undefined;
     }
 
-    // TODO: time attribute should only be the initial value
     // TODO: remove active attribute in favor of intersection observer
 
     static observedAttributes = ["time", "active"];
 
     attributeChangedCallback() {
       if (this.active && !this.intervalId) {
-        const initialTime = this.time;
-
         this.intervalId = setInterval(() => {
           const timeAttribute = this.time;
           const time = Number(timeAttribute);
 
           if (time > 0) {
-            this.time = String(time - 1);
+            this.time = time - 1;
             this.update();
           } else {
             this.active = false;
-            this.time = initialTime;
+            this.time = Number(this.getAttribute('time'));
 
             clearInterval(this.intervalId);
             this.intervalId = undefined;
@@ -77,7 +66,7 @@ customElements.define(
 
     update() {
       if (this.timeElement) {
-        this.timeElement.textContent = this.time;
+        this.timeElement.textContent = String(this.time);
       }
     }
   },
