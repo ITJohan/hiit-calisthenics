0a81435bad48c7bb9fbea5e9abadbc2f31fe0974
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-10-29 13:18:57 +0000

    feat: highlight first child

--- public/components/rr-range.js
+++ public/components/rr-range.js
@@ -21,13 +21,16 @@ customElements.define('rr-range', class RRRange extends HTMLElement {
   update() {
     this.innerHTML = `
       <ul>
-        ${this.reps.map(rep => `<li tabindex="0">${rep}</li>`).join('')}
+        ${this.reps.map((rep, index) => `<li tabindex="0" ${index === 0 ? 'class="active"' : ''}>${rep}</li>`).join('')}
       </ul> 
     `
     const container = this.querySelector('ul');
+    if (container === null) throw new Error('can not find the container')
 
     container.addEventListener('scrollsnapchanging', (event) => {
-      Array.from(event.target.children).forEach(child => child.classList.remove('active'))
+      const target = /** @type {HTMLUListElement} */ (event.target);
+      Array.from(target.children).forEach(child => child.classList.remove('active'))
+      // @ts-ignore: snapTargetInline is newly available
       event.snapTargetInline.classList.add('active')
     });
   }
