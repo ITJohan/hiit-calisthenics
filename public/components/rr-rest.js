ad34597a85175ec3a6bd1600412232fceca2d1fb
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-11-03 17:46:23 +0100

    feat: extract chevrons to own components

--- public/components/rr-rest.js
+++ public/components/rr-rest.js
@@ -10,21 +10,11 @@ customElements.define(
       this.innerHTML = `
         <h2>Rest</h2>
         <div>
-          <svg width="100" height="100">
-            <line x1="95" y1="5" x2="50" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="50" x2="95" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="5" x2="5" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="5" y1="50" x2="50" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-          </svg>
+          <rr-chevron-left></rr-chevron-left>
           <div>
             <p>Ad</p>
           </div>
-          <svg width="100" height="100">
-            <line x1="5" y1="5" x2="50" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="50" x2="5" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="5" x2="95" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="95" y1="50" x2="50" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-          </svg>
+          <rr-chevron-right></rr-chevron-right>
         </div>
         <div>Rest: <time></time></div>
   `;
--- public/components/rr-set.js
+++ public/components/rr-set.js
@@ -47,19 +47,9 @@ customElements.define(
       this.innerHTML = `
         <h2>${exercise.name}</h2>
         <div>
-          <svg width="100" height="100">
-            <line x1="95" y1="5" x2="50" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="50" x2="95" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="5" x2="5" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="5" y1="50" x2="50" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-          </svg>
+          <rr-chevron-left></rr-chevron-left>
           <img src="./assets/placeholder.jpg" alt="${exercise.name}" />
-          <svg width="100" height="100">
-            <line x1="5" y1="5" x2="50" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="50" x2="5" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="50" y1="5" x2="95" y2="50" stroke="white" stroke-width="10" stroke-linecap="round" />
-            <line x1="95" y1="50" x2="50" y2="95" stroke="white" stroke-width="10" stroke-linecap="round" />
-          </svg>
+          <rr-chevron-right></rr-chevron-right>
         </div>
         <rr-range name="${exercise.id}-${id}" reps="${reps.join(",")}"></rr-range>
       `;
